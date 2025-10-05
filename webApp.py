4494b574ee7c2c7846f1be859e739bc66c09574a
聪 <505810824@qq.com>
2025-10-05 20:37:26 +0800

    first commit

---
+++ webApp.py
@@ -0,0 +1,64 @@
+# import os
+# os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+import json
+import os
+import webbrowser
+
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+import lambdaGetSample
+import lambdaSpeechToScore
+import lambdaTTS
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = '*'
+
+rootPath = ''
+
+
+@app.route(rootPath+'/')
+def main():
+    return render_template('main.html')
+
+
+@app.route(rootPath+'/getAudioFromText', methods=['POST'])
+def getAudioFromText():
+    event = {'body': json.dumps(request.get_json(force=True))}
+    return lambdaTTS.lambda_handler(event, [])
+
+
+@app.route(rootPath+'/getSample', methods=['POST'])
+def getNext():
+    event = {'body':  json.dumps(request.get_json(force=True))}
+    return lambdaGetSample.lambda_handler(event, [])
+
+
+@app.route(rootPath+'/GetAccuracyFromRecordedAudio', methods=['POST'])
+def GetAccuracyFromRecordedAudio():
+
+    try:
+        event = {'body': json.dumps(request.get_json(force=True))}
+        lambda_correct_output = lambdaSpeechToScore.lambda_handler(event, [])
+    except Exception as e:
+        print('Error: ', str(e))
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Credentials': "true",
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': ''
+        }
+
+    return lambda_correct_output
+
+
+if __name__ == "__main__":
+    language = 'de'
+    print(os.system('pwd'))
+    webbrowser.open_new('http://127.0.0.1:3000/')
+    app.run(host="0.0.0.0", port=3000)
